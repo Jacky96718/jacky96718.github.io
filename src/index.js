213227624056cd22f61f96d2d3ba2d22e0a98af7
Hon Ting Wong <30693275+Jacky96718@users.noreply.github.com>
2019-06-24 01:03:48 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import "./styles.css";
 import PlayerClass from "./playerClass.js";
 
 var cvs, ctx, cvsHeight, cvsWidth;
 var player = new PlayerClass();
+var bg = new Image();
+bg.src="./src/img/bg.png"
 
 setup();
 document.addEventListener("mousemove", mouseHandler, false);
-requestAnimationFrame(gameLoop())
+requestAnimationFrame(gameLoop)
 
 function setup() {
   cvs = document.getElementById("gameCanvas");
@@ -17,16 +18,16 @@ function setup() {
 
 function mouseHandler(e) {
   player.xCoordinate = e.clientX - cvs.offsetLeft;
-  console.log("Spaceship x-coordinate = " + player.xCoordinate);
   //requestAnimationFrame(function(){player.draw(ctx)})
 }
 
 function gameLoop() {
-  ctx.clearRect(0, 0, cvsHeight, cvsWidth);
   draw();
-  requestAnimationFrame(gameLoop())
+  ctx.clearRect(0, 0, cvsHeight, cvsWidth);
+  requestAnimationFrame(gameLoop)
 }
 
 function draw() {
   player.draw(ctx);
+  //requestAnimationFrame(draw);
 }
